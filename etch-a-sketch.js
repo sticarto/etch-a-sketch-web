ec080705048d5ec281108719a7f47eaf26e1e381
Sticarto <123905512+sticarto@users.noreply.github.com>
2024-08-07 13:49:59 -0700

    Allow user to change the grid size based on width

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,12 +1,49 @@
 // Create 16 divs and append them to the html div
 const container = document.querySelector('#container');
 
-let gridSize = 16 * 16
-for (let i = 0; i < gridSize; i++) {
-    const div = document.createElement("div");
-    div.addEventListener('mouseover', () => {
-        div.style['backgroundColor'] = 'black';
-    });
-    container.appendChild(div);
+// Turn this into a function that creates a new grid based on
+// the given size
+function createGrid (gridSize) {
+    if (gridSize > 100) return;
 
+    let newGridSize = gridSize * gridSize;
+    container.style.width = (gridSize * 16) + 'px';
+
+    // This will clear all elements inside the container if it has any
+    let child = container.lastElementChild;
+    while (child) {
+        container.removeChild(child);
+        child = container.lastElementChild;
+    }
+
+    for (let i = 0; i < newGridSize; i++) {
+        const div = document.createElement("div");
+        div.addEventListener('mouseover', () => {
+            div.style['backgroundColor'] = 'black';
+        });
+        container.appendChild(div);
+    
+    }
 }
+
+// let gridSize = 16 * 16;
+// for (let i = 0; i < gridSize; i++) {
+//     const div = document.createElement("div");
+//     div.addEventListener('mouseover', () => {
+//         div.style['backgroundColor'] = 'black';
+//     });
+//     container.appendChild(div);
+
+// }
+
+function setGridSize () {
+    newSize = prompt('Enter a width and height (max 100)');
+    createGrid(newSize);
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    setGridSize();
+})
+
+createGrid(16);
